67ae931c7376aa91758e9b669cdcd7a6c4eb237f
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-02-26 13:18:44 +0530

    javascript practice

---
+++ array.js
@@ -0,0 +1,81 @@
+// let stu=["Aniruddh",18,6.0,"chw",true]
+// console.log(stu)
+
+// let stu=["Aniruddh",18,6.0,"chw",true]
+// for(let a=0;a<5;a++)
+// {
+//     console.log(stu[a])
+// }
+
+// let p=[]
+// for(let i=0;i<5;i++)
+// {
+//     p[i]=prompt("Enter value")
+// }
+// console.log(p)
+
+// wap to print a sum of all elements between 8 to 1
+
+// let p=[8,7,6,5,4,3,2,1]
+// let sum=0
+// for(let i=0;i<(p.length);i++)
+// {
+//     sum=sum+p[i]
+// }
+// console.log(sum)
+
+
+// (...) spread operator
+
+// let ar1=["Virat","Dhoni","Rohit","Hardik","Jadeja"]
+// let ar2=["Abhishek","Shubhman","Bumrah"]
+// ar3=[...ar1,...ar2]
+// console.log(ar3)
+
+// destructuring arry acces through variable not indexing
+
+// let [name,age,city,height] =["Virat",35,"delhi",5.9]
+// console.log(name);
+
+
+// let [name,age,,height] =["Virat",35,"delhi",5.9]
+// console.log(height);
+
+
+// for of loop
+
+// let emp =["Deepali","bhpl",341241241,"Cybrom"]
+
+//     for(let v of emp)
+//     {
+//         console.log(v)
+//     }
+
+// let ar =[1,2,3,4,5,6,7,8,9,10]
+// for(let v of ar)
+// {
+//     if(v%2==0)
+//     {
+//         console.log(v*v)
+//     }
+// }
+
+ 
+// let stu=["Aniruddh","Unmarried",18,"65kg","Indore"]
+// console.log(stu)
+
+// let ar9 = stu.slice(1,4)
+// console.log(ar9)
+
+
+
+
+// let stu=["Aniruddh","Unmarried",18,"65kg","Indore"]
+// stu.splice(index no, no of elements,element)
+
+// stu.splice(2,3)
+// console.log(stu)
+
+// stu.splice(2,3,"orange")
+// console.log(stu)
+
