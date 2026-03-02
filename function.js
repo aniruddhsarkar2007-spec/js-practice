c889e81cea2110c4d27bb2999e9d5a89a6a7ce09
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-02 14:34:59 +0530

    js practice

---
+++ function.js
@@ -0,0 +1,95 @@
+// FUNCTION IS BLOCK OF CODE AND SET OF INSTRUCTION
+
+// SYNTAX
+// function identifiers()
+//
+//  { 
+//    code
+//  }
+
+// function is made by this-
+
+// function decleration
+// function defination
+// function calling
+
+// function demo()
+// {
+//     let a=parseInt(prompt("Enter no:"))
+//     let b=parseInt(prompt("Enter no:"))
+//     console.log(a+b)
+// }
+//  demo()
+
+
+// types of function
+
+// user defined
+// pre defined
+
+
+//--------------------------------------
+//named function
+//anonymus function- Which doesnt have any name (Arrow function)
+
+// Types of user deffined function
+
+// Without argument and no return value 
+// With argument and no return value 
+// With argument and return value 
+// With argument and with return value 
+
+
+
+// ARG AND PARAMETER
+
+// function fun1(PARAMETER)
+// {
+
+// }
+// fun1(Value)
+// the values inside function calling is argument
+
+// function demo(a){
+//     return a+a
+// }
+// demo(8)
+
+
+//with argument and no return
+
+// function add(a){
+//   console.log(a+a)
+// }
+// add(4)
+
+// wap to print a table of any no using function type 2 with argument and no return
+
+// function table(a)
+// {
+    
+// for(let n=1;n<=10;n++)
+// {
+//     console.log(a*n)
+// }
+
+// }
+// let a=parseInt(prompt("Enter no:"))
+// table(a)
+
+
+
+// function hii()
+// {
+//     let a=5
+//     return a+a
+// }
+// console.log(hii())
+
+
+function hii(a)
+{
+  return a+a
+}
+
+console.log(hii(1))
