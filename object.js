fba612c894f8871cd8f04bca1d5358ee4d06a68f
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-05 13:30:52 +0530

    Javascript practice

---
+++ object.js
@@ -0,0 +1,92 @@
+// { key : value} -> unordered (indexing x)
+
+// let student ={name:"Code",age:34,city:"Bhopal",
+//     bank:{
+//         acno:12345,ac_balance:235321
+//     },
+
+//     monthly:[234,352,5434]
+
+// }
+
+// console.log(student)
+
+
+// let emp={
+//     emp_name:prompt("Enter name:"),
+//     emp_age:prompt("Enter Age:"),
+//     emp_city:prompt("Enter City:"),
+
+// }
+// console.log(emp)
+
+
+//for in loop is use to print key
+
+// let ob={
+//     a:"hii",
+//     b:"how are u",
+//     c:"byee"
+// }
+
+// for(let k in ob)
+// {
+//     console.log(k)
+// }
+
+// . notation
+// [] notation
+
+// let ob={
+//     "name 1":"Ali",
+//     "name 2":"Khan"
+// }
+
+// console.log(ob["name 1"]);
+
+
+
+// let ob={
+//     "name 1":"Ali",
+//     "name 2":"Khan"
+// }
+// for (let k in ob)
+// {
+//     if(k=="name 1")
+//     {
+//         console.log(k)
+//     }
+// }
+
+
+// let ob={
+//     "name 1":"Ali",
+//     "name 2":"Khan"
+// }
+// let k=Object.keys(ob)
+// console.log(k[0]);
+
+
+
+//DESTRUCTURING OF OBJECT
+
+
+// let {name,city}={
+//     name:"code",
+//     city:"bpl"
+// }
+// console.log(name);
+
+
+// let ob1={
+//     name:"code",
+//     city:"bpl"
+// }
+// let ob2={
+//     name1:"jhon",
+//     city1:"chw"
+// }
+
+// let ob3={...ob1,...ob2}
+
+// console.log(ob3);
