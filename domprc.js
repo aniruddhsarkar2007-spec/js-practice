07f39f8d1bf4f307bc2c83193fe8c333ae79fd09
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-13 15:09:54 +0530

    js practice

---
+++ domprc.js
@@ -0,0 +1,15 @@
+function cl1(){
+    document.body.style.backgroundColor="red"
+}
+function cl2(){
+    document.body.style.backgroundColor="yellow"
+}
+function cl3(){
+    document.body.style.backgroundColor="pink"
+}
+function cl4(){
+    document.body.style.backgroundColor="blue"
+}
+function cl5(){
+    document.body.style.backgroundColor="black"
+}
