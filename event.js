2adf4924097198e53ed8603ec8e541550b98d46e
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-15 22:58:34 +0530

    learning js

---
+++ event.js
@@ -0,0 +1,23 @@
+// events are the action performed by the user
+
+// function fun()
+// {
+//     alert('working')
+// }
+
+// function hii()
+// {
+//     let a=parseInt(prompt("enter no:"))
+//     let b=parseInt(prompt("enter no:"))
+//     alert(a+b)
+// }
+
+// function demo()
+// {
+//     document.write("<h1> hii how r you</h1>")
+// }
+
+function demo()
+{
+    document.getElementById('res').textContent="hii"
+}
